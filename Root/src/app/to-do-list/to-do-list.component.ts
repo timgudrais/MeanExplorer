5b5fd113facff0522b275041cd1ad4e5b4f3c55d
Tim G <tim.gudrais@gmail.com>
2017-02-09 22:10:31 +0100

    Commit.

---
+++ Root/src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent implements OnInit {
+  title = 'To do';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
