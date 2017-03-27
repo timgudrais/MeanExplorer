376905ab9df0bbf3ac6f8badf61b748b7686d91a
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-03-27 21:22:51 +0200

    Everything is AWESOME! EVERYTHING IS COOL WHEN YOUR PART OF THE TEAM!

--- Root/src/app/small-cap/small-cap.component.ts
+++ Root/src/app/small-cap/small-cap.component.ts
@@ -15,15 +15,56 @@ export class SmallCapComponent implements OnInit {
   }
 
   constructor(private userService: UserService) {}
-  profile = {};
-  headers;
+  dataDump;
+  headersArray = [];
+  headers = "";
+  rowArray = [];
+  dataLoaded = false;
+  showErrorMessage = false;
+  displayTable = false;
 
-  loadUser() {
-    this.userService.getData().subscribe(data => this.profile = data);
+  loadData() {
+    // this.userService.getData().subscribe(data => this.dataDump = data);
+    this.userService.getData().subscribe(data => this.dataDump = data.query.results.table);
+    this.dataLoaded = true;
   }
 
-  loadHeaders() {
-    this.userService.getData().subscribe(data => this.profile = data.query.results.table.thead.tr.th);
-    this.headers = this.profile[2];
+  loadHeaders() {    
+    if(this.dataDump === undefined){
+      this.showErrorMessage = true;      
+      return;
+    }
+    else
+    {                 
+      this.showErrorMessage = false;    
+                    
+      for (var i = 0; i < this.dataDump.thead.tr.th.length; i++) { 
+        var tempString = this.dataDump.thead.tr.th[i].content;
+        // this.headers = tempString.replace(/['"]+/g, '');  
+        this.headersArray.push(tempString);       
+      }
+
+      for (var i = 0; i < this.dataDump.tbody.tr.length; i++) { 
+        var row = [];
+        for (var x = 0; x < this.dataDump.tbody.tr[i].td.length; x++) {
+          var tempstring;
+          if(this.dataDump.tbody.tr[i].td[x].a !== undefined && this.dataDump.tbody.tr[i].td[x].a.content !== undefined) {                        
+            tempString = this.dataDump.tbody.tr[i].td[x].a.content;            
+          }
+          else if(this.dataDump.tbody.tr[i].td[x].a !== undefined && this.dataDump.tbody.tr[i].td[x].a.href !== undefined) {
+            tempString = this.dataDump.tbody.tr[i].td[x].a.href; 
+          }
+          else{
+            tempString = this.dataDump.tbody.tr[i].td[x].content;
+          }
+          row.push(tempString);      
+        }
+        this.rowArray.push(row);  
+      }
+      
+      this.displayTable = true;
+    }
+    
+    // this.headers = this.dataDump.query.results.table.thead.tr.th;
   }
 }
