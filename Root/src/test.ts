26fe3fee1c21c1f6f1fc60200842f3c136bfa288
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-03-27 18:49:52 +0200

    test2

--- Root/src/test.ts
+++ Root/src/test.ts
@@ -24,6 +24,8 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
   
+  
+  
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
