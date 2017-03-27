20336dafee48e361fab7ce28bf23104996490289
Sebastian Uddén <sebastian.udden@eurostep.com>
2017-03-27 19:18:00 +0200

    Started being awesome with YQL

--- Root/src/app/small-cap/small-cap.component.ts
+++ Root/src/app/small-cap/small-cap.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service'
 
 @Component({
   selector: 'app-small-cap',
   templateUrl: './small-cap.component.html',
-  styleUrls: ['./small-cap.component.css']
+  styleUrls: ['./small-cap.component.css'],
+  providers: [
+    UserService
+  ]
 })
 export class SmallCapComponent implements OnInit {
   title = "Small Cap";
-
-  constructor() { }
-
   ngOnInit() {
   }
 
+  constructor(private userService: UserService) {}
+  profile = {};
+  headers;
+
+  loadUser() {
+    this.userService.getData().subscribe(data => this.profile = data);
+  }
+
+  loadHeaders() {
+    this.userService.getData().subscribe(data => this.profile = data.query.results.table.thead);
+    this.headers = this.profile;
+  }
 }
