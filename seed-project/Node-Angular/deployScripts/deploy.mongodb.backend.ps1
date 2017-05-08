4cbf56e16231cbf469faec3c36f6a2bfa99d21a3
Tim G <tim.gudrais@gmail.com>
2017-05-09 00:15:59 +0200

    Fixat cloud

--- seed-project/Node-Angular/deployScripts/deploy.mongodb.backend.ps1
+++ seed-project/Node-Angular/deployScripts/deploy.mongodb.backend.ps1
@@ -1,5 +1,5 @@
-$MongoDexePath = "C:\Users\sun\Desktop\DevTest\MeanExplorer\seed-project\MongoDb\bin\mongod.exe "
-$MongoDatabasePath = "--dbpath C:\Users\sun\Desktop\DevTest\MeanExplorer\seed-project\MongoDb\data"
+$MongoDexePath = "C:\Users\Tim\Documents\GitProjects\MeanExplorer\seed-project\MongoDb\bin\mongod.exe "
+$MongoDatabasePath = "--dbpath C:\Users\Tim\Documents\GitProjects\MeanExplorer\seed-project\MongoDb\data"
 $FullMongoDeployCommand = $MongoDexePath + $MongoDatabasePath
 
 Invoke-Expression $FullMongoDeployCommand
