60bcfd8ee819cfc7036db31adf4f5c80a2b79278
Tim G <tim.gudrais@gmail.com>
2017-05-10 20:17:48 +0200

    hej

--- seed-project/Node-Angular/deployScripts/deploy.all.fullstack.ps1
+++ seed-project/Node-Angular/deployScripts/deploy.all.fullstack.ps1
@@ -1,5 +1,4 @@
 $SeedProjectPath = "$PSScriptRoot"
-$MongoDbPath = $SeedProjectPath + '\deploy.mongodb.backend.ps1'
 $NodeJSPath = $SeedProjectPath + '\deploy.node.backend.ps1'
 $AngularPath = $SeedProjectPath + '\deploy.angular.frontend.ps1'
 
@@ -8,8 +7,6 @@ Write-Host $NodeJSPath
 Write-Host $AngularPath
 
 Set-Location -Path "$PSScriptRoot"
-Write-Host "Deploying MongoDb" -foregroundcolor Magenta
-Invoke-Expression 'cmd /c start powershell.exe -nologo -noexit -command { Invoke-Expression C:\Users\Tim\Documents\GitProjects\MeanExplorer\seed-project\Node-Angular\deployScripts\deploy.mongodb.backend.ps1 }'
 
 Write-Host "Deploying NodeJS" -foregroundcolor Magenta
 Invoke-Expression 'cmd /c start powershell.exe -nologo -noexit -command { Invoke-Expression C:\Users\Tim\Documents\GitProjects\MeanExplorer\seed-project\Node-Angular\deployScripts\deploy.node.backend.ps1 }'
