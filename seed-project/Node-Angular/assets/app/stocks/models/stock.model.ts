a99b4407e791491ab00602a8e39ce7f0d012e4bd
Tim G <tim.gudrais@gmail.com>
2017-09-24 23:59:41 +0200

    HEJ

--- seed-project/Node-Angular/assets/app/stocks/models/stock.model.ts
+++ seed-project/Node-Angular/assets/app/stocks/models/stock.model.ts
@@ -5,189 +5,484 @@ export class StockPriceObject {
 export class StockObject {
     Info: Info;
     Stock: Stock;
-    Valuation: Valuation;
-    TA: TA;
-    Strat: Strat;
+    BalanceSheet: BalanceSheet;
+    IncomeStatement: IncomeStatement;
     Profitability: Profitability;
-    BalanceSheet: BalanceSheet;    
+    Efficiency: Efficiency;
+    Insider: Insider;
+    Strategy: Strategy;
+    TA: TA;
+    Valuation: Valuation;
+       
 
     constructor(
         Info: Info, 
         Stock: Stock, 
-        Valuation: Valuation,
+        BalanceSheet: BalanceSheet,
+        IncomeStatement: IncomeStatement,
+        Profitability: Profitability, 
+        Efficiency: Efficiency,
+        Insider: Insider,
+        Strategy: Strategy, 
         TA: TA,
-        Strat: Strat,
-        Profitability: Profitability,
-        BalanceSheet: BalanceSheet,        
+        Valuation: Valuation
         ) {
             this.Info = Info;
             this.Stock = Stock;
-            this.Valuation = Valuation;
-            this.TA = TA;
-            this.Strat = Strat;
+            this.BalanceSheet = BalanceSheet;
+            this.IncomeStatement = IncomeStatement;
             this.Profitability = Profitability;
-            this.BalanceSheet = BalanceSheet;            
+            this.Efficiency = Efficiency;
+            this.Insider = Insider;  
+            this.Strategy = Strategy;     
+            this.TA = TA;   
+            this.Valuation = Valuation;
         }
+}
+
+export class Average {
+    Actual_1y: number;
+    Actual_3y: number;
+
+    constructor(
+        Actual_1y: number,
+        Actual_3y: number
+    ) {
+        this.Actual_1y = Actual_1y;
+        this.Actual_3y = Actual_3y
+    }
+
+}
+
+export class Growth {
+    Actual_1y: number;
+
+    constructor(
+        Actual_1y: number
+    ) {
+        this.Actual_1y = Actual_1y;
+    }
+}
+
+export class GrowthPlusQ {
+    Actual_1q: number;
+    Actual_1y: number;
+
+    constructor(
+        Actual_1q: number,
+        Actual_1y: number
+    ) {
+        this.Actual_1q = Actual_1q;
+        this.Actual_1y = Actual_1y;
+    }
 }
 
 export class Info {
     ISIN: string;
     CompanyDescription: string;
     CompanyName: string;
-    Ticker: string;
     Country: string;
-    List: string;
     Industry: string;
-    // ReportDate: ReportDate;
+    List: string;
+    Ticker: string;
 
     constructor(
         ISIN: string, 
         CompanyDescription: string,
         CompanyName: string, 
-        Ticker: string, 
         Country: string, 
-        List: string, 
         Industry: string, 
-        // ReportDate: ReportDate
+        List: string, 
+        Ticker: string, 
         ) {
             this.ISIN = ISIN;
             this.CompanyDescription = CompanyDescription;
             this.CompanyName = CompanyName;
-            this.Ticker = Ticker;
             this.Country = Country;
-            this.List = List;
             this.Industry = Industry;
-            // this.ReportDate = ReportDate;
+            this.List = List;
+            this.Ticker = Ticker;
     }
 }
 
-// export class ReportDate {
-//     Next: string;
-
-//     constructor(
-//         Next: string
-//         ) {
-//             this.Next = Next;
-//     }
-// }
-
 export class Stock {
-    LatestPrice: number;
+    Price: Price;
     Return: Return;
+    MarketCap: string;
+    NrShares: NrShares;
 
     constructor(
-        LatestPrice: number, 
-        Return: Return
+        Price: Price, 
+        Return: Return,
+        MarketCap: string,
+        NrShares: NrShares
         ) {
-            this.LatestPrice = LatestPrice;
+            this.Price = Price;
             this.Return = Return;
+            this.MarketCap = MarketCap;
+            this.NrShares = NrShares;
+    }
+}
+
+export class Price {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
     }
 }
 
 export class Return {
-    // Act_1d: number;
-    // Act_1w: number;
-    // Act_1m: number;
-    Act_3m: number;
-    // Act_6m: number;
-    // Act_1y: number;
-    // Act_3y: number;
-    // Act_5y: number;
+    Actual_3m: number;
 
     constructor(
-        // Act_1d: number, 
-        // Act_1w: number, 
-        // Act_1m: number, 
-        Act_3m: number, 
-        // Act_6m: number, 
-        // Act_1y: number,
-        // Act_3y: number,
-        // Act_5y: number
+        Actual_3m: number, 
         ) {
-            // this.Act_1d = Act_1d;
-            // this.Act_1w = Act_1w;
-            // this.Act_1m = Act_1m;
-            this.Act_3m = Act_3m;
-            // this.Act_6m = Act_6m;
-            // this.Act_1y = Act_1y;
-            // this.Act_3y = Act_3y;
-            // this.Act_5y = Act_5y;
+            this.Actual_3m = Actual_3m;
     }
 }
 
-export class Valuation {
-    MarketCap: number;
-    P_E: P_E;
-    P_S: P_S;
-    P_FCF: P_FCF;
+export class NrShares {
+    Latest: number;
+    Growth: Growth;
 
     constructor(
-        MarketCap: number, 
-        P_E: P_E, 
-        P_S: P_S, 
-        P_FCF: P_FCF
-        ) {
-            this.MarketCap = MarketCap;
-            this.P_E = P_E;
-            this.P_S = P_S;
-            this.P_FCF = P_FCF;
-    }
-}
-
-export class P_E {
-    Latest: number;
-    Avg_1y: number;
-    Avg_3y: number;
-
-    constructor(Latest: number, Avg_1y: number, Avg_3y: number) {
+        Latest: number,
+        Growth: Growth
+    ) {
         this.Latest = Latest;
-        this.Avg_1y = Avg_1y;
-        this.Avg_3y = Avg_3y;
+        this.Growth = Growth
     }
 }
 
-export class P_S {
-    Latest: number;
-    Avg_1y: number;
-    Avg_3y: number;
-
-    constructor(Latest: number, Avg_1y: number, Avg_3y: number) {
-        this.Latest = Latest;
-        this.Avg_1y = Avg_1y;
-        this.Avg_3y = Avg_3y;
-    }
-}
-
-export class P_FCF {
-    Latest: number;
-    Avg_1y: number;
-    Avg_3y: number;
-
-    constructor(Latest: number, Avg_1y: number, Avg_3y: number) {
-        this.Latest = Latest;
-        this.Avg_1y = Avg_1y;
-        this.Avg_3y = Avg_3y;
-    }
-}
-
-export class TA {
-    MA5_MA20: number;
-    MA20_MA70: number;
-    MA50_MA200: number;
+export class BalanceSheet {
+    AssetTurnover: AssetTurnover;
+    CurrentRatio: CurrentRatio;    
+    DebtPerShare: DebtPerShare;
+    Equity: Equity;
+    EquityPerShare: EquityPerShare;
+    NetDebt: NetDebt;
+    TotalAssets: TotalAssets;
 
     constructor(
-        MA5_MA20: number, 
-        MA20_MA70: number, 
-        MA50_MA200: number
-        ) {
-            this.MA5_MA20 = MA5_MA20;
-            this.MA20_MA70 = MA20_MA70;
-            this.MA50_MA200 = MA50_MA200;
+        AssetTurnover: AssetTurnover,
+        CurrentRatio: CurrentRatio,  
+        DebtPerShare: DebtPerShare,
+        Equity: Equity,
+        EquityPerShare: EquityPerShare,
+        NetDebt: NetDebt,
+        TotalAssets: TotalAssets,
+    ) {
+        this.AssetTurnover = AssetTurnover;
+        this.CurrentRatio = CurrentRatio;
+        this.DebtPerShare = DebtPerShare;
+        this.Equity = Equity;
+        this.EquityPerShare = EquityPerShare;
+        this.NetDebt = NetDebt;
+        this.TotalAssets = TotalAssets;
     }
 }
 
-export class Strat {
-    Graham: number;
+export class AssetTurnover {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
+    }
+}
+
+export class CurrentRatio {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class DebtPerShare {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
+    }
+}
+
+export class Equity {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class EquityPerShare {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
+    }
+}
+
+export class NetDebt {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class TotalAssets {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class IncomeStatement {
+    NetProfit: NetProfit;
+    Revenue: Revenue;
+
+    constructor(
+        NetProfit: NetProfit,
+        Revenue: Revenue
+    ) {
+        this.NetProfit = NetProfit;
+        this.Revenue = Revenue
+    }
+}
+
+export class NetProfit {
+    Growth: Growth;
+
+    constructor(
+        Growth: Growth,
+    ) {
+        this.Growth = Growth
+    }
+}
+
+export class Revenue {
+    Growth: Growth;
+
+    constructor(
+        Growth: Growth,
+    ) {
+        this.Growth = Growth
+    }
+}
+
+export class Profitability {
+    CashFlowOperations: CashFlowOperations;
+    Dividend: Dividend;
+    DividendRatio: DividendRatio;
+    ROAG: ROAG;
+    ROC: ROC;
+    ROE: ROE;
+
+    constructor(
+        CashFlowOperations: CashFlowOperations,
+        Dividend: Dividend,
+        DividendRatio: DividendRatio,
+        ROAG: ROAG,
+        ROC: ROC,
+        ROE: ROE
+    ) {
+        this.CashFlowOperations = CashFlowOperations;
+        this.Dividend = Dividend;
+        this.DividendRatio = DividendRatio;
+        this.ROAG = ROAG;
+        this.ROC = ROC;
+        this.ROE = ROE
+    }
+}
+
+export class CashFlowOperations {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class Dividend {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class DividendRatio {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
+    }
+}
+
+export class ROAG {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class ROC {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class ROE {
+    Latest: number;
+
+    constructor(
+        Latest: number,
+    ) {
+        this.Latest = Latest
+    }
+}
+
+export class Efficiency {
+    EBITMargin: EBITMargin;
+    GrossMargin: GrossMargin;
+    NetProfitMargin: NetProfitMargin;
+
+    constructor(
+        EBITMargin: EBITMargin,
+        GrossMargin: GrossMargin,
+        NetProfitMargin: NetProfitMargin,
+    ) {
+        this.EBITMargin = EBITMargin;
+        this.GrossMargin = GrossMargin;
+        this.NetProfitMargin = NetProfitMargin
+    }
+}
+
+export class EBITMargin {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class GrossMargin {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class NetProfitMargin {
+    Latest: number;
+    Growth: Growth;
+
+    constructor(
+        Latest: number,
+        Growth: Growth
+    ) {
+        this.Latest = Latest;
+        this.Growth = Growth
+    }
+}
+
+export class Insider {
+    Net: Net;
+
+    constructor (
+        Net: Net
+    ) {
+        this.Net = Net
+    }
+}
+
+export class Net {
+    Actual_1w: number;
+    Actual_1m: number;
+    Actual_3m: number;
+    Actual_1y: number;
+
+    constructor (
+        Actual_1w: number,
+        Actual_1m: number,
+        Actual_3m: number,
+        Actual_1y: number
+    ) {
+        this.Actual_1w = Actual_1w;
+        this.Actual_1m = Actual_1m;
+        this.Actual_3m = Actual_3m;
+        this.Actual_1y = Actual_1y
+    }
+}
+
+export class Strategy {
+    GrahamScore: number;
 
     DividendRank: number;  
     PERank: number;
@@ -206,9 +501,9 @@ export class Strat {
     InvestingScore: number;  
 
     constructor(
-        Graham: number        
+        GrahamScore: number        
         ) {
-            this.Graham = Graham;
+            this.GrahamScore = GrahamScore;
             
             this.DividendRank = 0;
             this.PERank = 0;
@@ -228,45 +523,73 @@ export class Strat {
         }
 }
 
-export class Profitability {
-    ROC: number;
-    GrossMargin: number;
-    EBITMargin: number;
-    NetMargin: number;
-    ROE: number;
-    CurrentRatio: number;
-    DividendRatio: number;
-    DebtEquity: number;
+export class TA {
+    MA5_MA20: number;
+    MA20_MA70: number;
+    MA50_MA200: number;
 
     constructor(
-        ROC: number,
-        GrossMargin: number,
-        EBITMargin: number,
-        NetMargin: number,
-        ROE: number,
-        CurrentRatio: number,
-        DividendRatio: number     
-    ) {
-        this.ROC = ROC;
-        this.GrossMargin = GrossMargin;
-        this.EBITMargin = EBITMargin;
-        this.NetMargin = NetMargin;
-        this.ROE = ROE;
-        this.CurrentRatio = CurrentRatio;
-        this.DividendRatio = DividendRatio;
-        this.DebtEquity = 0;        
+        MA5_MA20: number, 
+        MA20_MA70: number, 
+        MA50_MA200: number
+        ) {
+            this.MA5_MA20 = MA5_MA20;
+            this.MA20_MA70 = MA20_MA70;
+            this.MA50_MA200 = MA50_MA200;
     }
 }
 
-export class BalanceSheet {
-    DebtPerShare: number;
-    EquityPerShare: number;
+export class Valuation {
+    P_E: P_E;
+    P_S: P_S;
+    P_FCF: P_FCF;
 
     constructor(
-        DebtPerShare: number,
-        EquityPerShare: number
-    ) {
-        this.DebtPerShare = DebtPerShare;
-        this.EquityPerShare = EquityPerShare;
+        P_E: P_E, 
+        P_S: P_S, 
+        P_FCF: P_FCF
+        ) {
+            this.P_E = P_E;
+            this.P_S = P_S;
+            this.P_FCF = P_FCF;
+    }
+}
+
+export class P_E {
+    Latest: number;
+    Average: Average;
+
+    constructor(
+        Latest: number,
+        Average: Average
+        ) {
+            this.Latest = Latest;
+            this.Average = Average;
+    }
+}
+
+export class P_S {
+    Latest: number;
+    Average: Average;
+
+    constructor(
+        Latest: number,
+        Average: Average
+        ) {
+            this.Latest = Latest;
+            this.Average = Average;
+    }
+}
+
+export class P_FCF {
+    Latest: number;
+    Average: Average;
+
+    constructor(
+        Latest: number,
+        Average: Average
+        ) {
+            this.Latest = Latest;
+            this.Average = Average;
     }
 }
--- seed-project/Node-Angular/assets/app/stocks/stock.service.ts
+++ seed-project/Node-Angular/assets/app/stocks/stock.service.ts
@@ -3,7 +3,7 @@ import { Injectable, EventEmitter } from "@angular/core";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
 
-import { StockObject, Info, Stock, Return, Valuation, P_E, P_S, P_FCF, TA, Strat, Profitability, BalanceSheet } from "./models/stock.model";
+import { StockObject, Info, Stock, Return, Valuation, P_E, P_S, P_FCF, TA, Strategy, Profitability, BalanceSheet, IncomeStatement, Price, NrShares, Growth, AssetTurnover, CurrentRatio, DebtPerShare, Equity, EquityPerShare, NetDebt, Net, NetProfit, NetProfitMargin, TotalAssets, Revenue, Dividend, DividendRatio, CashFlowOperations, ROC, EBITMargin, Efficiency, ROE, Insider, Average, GrossMargin, ROAG, GrowthPlusQ} from "./models/stock.model";
 
 @Injectable()
 export class StockService {
@@ -33,65 +33,165 @@ export class StockService {
                                 stock.Info.ISIN,
                                 stock.Info.CompanyDescription,
                                 stock.Info.CompanyName,
-                                stock.Info.Ticker,
                                 stock.Info.Country,
+                                stock.Info.Industry,
                                 stock.Info.List,
-                                stock.Info.Industry
-                                // new ReportDate(
-                                //     stock.Info.ReportDate.Next
-                                // )
+                                stock.Info.Ticker
                             ),
                             new Stock(
-                                stock.Stock.LatestPrice,
+                                new Price(
+                                    stock.Stock.Price.Latest
+                                ),
                                 new Return(
-                                    // stock.Stock.Return.Act_1d,
-                                    // stock.Stock.Return.Act_1w,
-                                    // stock.Stock.Return.Act_1m,
-                                    stock.Stock.Return.Act_3m
-                                    // stock.Stock.Return.Act_6m,
-                                    // stock.Stock.Return.Act_1y,
-                                    // stock.Stock.Return.Act_3y,
-                                    // stock.Stock.Return.Act_5y
+                                    stock.Stock.Return.Actual_3m
+                                ),
+                                stock.Stock.MarketCap,
+                                new NrShares(
+                                    stock.Stock.NrShares.Latest,
+                                    new Growth(
+                                        stock.Stock.NrShares.Growth.Actual_1y
+                                    )
                                 )
                             ),
-                            new Valuation(
-                                stock.Valuation.MarketCap,
-                                new P_E (
-                                    stock.Valuation.P_E.Latest,
-                                    stock.Valuation.P_E.Avg_1y,
-                                    stock.Valuation.P_E.Avg_3y
+                            new BalanceSheet(
+                                new AssetTurnover(
+                                    stock.BalanceSheet.AssetTurnover.Latest
                                 ),
-                                new P_S (
-                                    stock.Valuation.P_S.Latest,
-                                    stock.Valuation.P_S.Avg_1y,
-                                    stock.Valuation.P_S.Avg_3y
+                                new CurrentRatio(
+                                    stock.BalanceSheet.CurrentRatio.Latest,
+                                    new Growth(
+                                        stock.BalanceSheet.CurrentRatio.Growth.Actual_1y
+                                    )
                                 ),
-                                new P_FCF (
-                                    stock.Valuation.P_FCF.Latest,
-                                    stock.Valuation.P_FCF.Avg_1y,
-                                    stock.Valuation.P_FCF.Avg_3y
+                                new DebtPerShare(
+                                    stock.BalanceSheet.DebtPerShare.Latest
+                                ),
+                                new Equity(
+                                    stock.BalanceSheet.Equity.Latest,
+                                    new Growth(
+                                        stock.BalanceSheet.Equity.Growth.Actual_1y
+                                    )
+                                ),
+                                new EquityPerShare(
+                                    stock.BalanceSheet.EquityPerShare.Latest
+                                ),
+                                new NetDebt(
+                                    stock.BalanceSheet.NetDebt.Latest,
+                                    new Growth(
+                                        stock.BalanceSheet.NetDebt.Growth.Actual_1y
+                                    )
+                                ),
+                                new TotalAssets(
+                                    stock.BalanceSheet.TotalAssets.Latest,
+                                    new Growth(
+                                        stock.BalanceSheet.TotalAssets.Growth.Actual_1y
+                                    )
                                 )
+                            ),
+                            new IncomeStatement(
+                                new NetProfit(
+                                    new GrowthPlusQ(
+                                        stock.IncomeStatement.NetProfit.Growth.Actual_1q,
+                                        stock.IncomeStatement.NetProfit.Growth.Actual_1y
+                                    )
+                                ),
+                                new Revenue(
+                                    new GrowthPlusQ(
+                                        stock.IncomeStatement.Revenue.Growth.Actual_1q,
+                                        stock.IncomeStatement.Revenue.Growth.Actual_1y
+                                    )
+                                )
+                            ),
+                            new Profitability (
+                                new CashFlowOperations(
+                                        stock.Profitability.CashFlowOperatons.Latest,
+                                        new Growth(
+                                            stock.Profitability.CashFlowOperatons.Growth.Actual_1y
+                                        )
+                                ),
+                                new Dividend(
+                                        stock.Profitability.Dividend.Latest,
+                                        new Growth(
+                                            stock.Profitability.Dividend.Growth.Actual_1y
+                                        )
+                                ),
+                                new DividendRatio(
+                                        stock.Profitability.DividendRatio.Latest
+                                ),
+                                new ROAG(
+                                        stock.Profitability.ROAG.Latest,
+                                        new Growth(
+                                            stock.Profitability.ROAG.Growth.Actual_1y
+                                        )
+                                ),
+                                new ROC(
+                                        stock.Profitability.ROC.Latest,
+                                        new Growth(
+                                            stock.Profitability.ROC.Growth.Actual_1y
+                                        )
+                                ),
+                                new ROE(
+                                        stock.Profitability.ROE.Latest
+                                ),
+                            ),
+                            new Efficiency(
+                                new EBITMargin(
+                                    stock.Efficiency.EBITMargin.Latest,
+                                    new Growth(
+                                        stock.Efficiency.EBITMargin.Growth.Actual_1y
+                                    )
+                                ),
+                                new GrossMargin(
+                                    stock.Efficiency.GrossMargin.Latest,
+                                    new Growth(
+                                        stock.Efficiency.GrossMargin.Growth.Actual_1y
+                                    )
+                                ),
+                                new NetProfitMargin(
+                                    stock.Efficiency.NetProfitMargin.Latest,
+                                    new Growth(
+                                        stock.Efficiency.NetProfitMargin.Growth.Actual_1y
+                                    )
+                                )
+                            ),
+                            new Insider(
+                                new Net(
+                                    stock.Insider.Net.Actual_1w,
+                                    stock.Insider.Net.Actual_1m,
+                                    stock.Insider.Net.Actual_3m,
+                                    stock.Insider.Net.Actual_12m
+                                )
+                            ),
+                            new Strategy (
+                                stock.Strategy.GrahamScore
                             ),
                             new TA (
                                 stock.TA.MA5_MA20,
                                 stock.TA.MA20_MA70,
                                 stock.TA.MA50_MA200
                             ),
-                            new Strat (
-                                stock.Strat.Graham
-                            ),
-                            new Profitability (
-                                stock.Profitability.ROC,
-                                stock.Profitability.GrossMargin,
-                                stock.Profitability.EBITMargin,
-                                stock.Profitability.NetMargin,
-                                stock.Profitability.ROE,
-                                stock.Profitability.CurrentRatio,
-                                stock.Profitability.DividendRatio
-                            ),
-                            new BalanceSheet (
-                                stock.BalanceSheet.DebtPerShare,
-                                stock.BalanceSheet.EquityPerShare
+                            new Valuation(
+                                new P_E (
+                                    stock.Valuation.P_E.Latest,
+                                    new Average(
+                                        stock.Valuation.P_E.Average.Actual_1y,
+                                        stock.Valuation.P_E.Average.Actual_3y
+                                    )
+                                ),
+                                new P_S (
+                                    stock.Valuation.P_S.Latest,
+                                    new Average(
+                                        stock.Valuation.P_S.Average.Actual_1y,
+                                        stock.Valuation.P_S.Average.Actual_3y
+                                    )
+                                ),
+                                new P_FCF (
+                                    stock.Valuation.P_FCF.Latest,
+                                    new Average(
+                                        stock.Valuation.P_FCF.Average.Actual_1y,
+                                        stock.Valuation.P_FCF.Average.Actual_3y
+                                    )
+                                ),
                             )
                         )
                     );
